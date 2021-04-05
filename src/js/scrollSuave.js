a1d1fe9e72e2dba0ce30611b68d0729fff65d0b7
Your Name <you@example.com>
2021-04-05 12:14:45 -0300

    ..

--- src/js/scrollSuave.js
+++ src/js/scrollSuave.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $('a[href^="#main"]').on('click',function (e) {
+    $('a[href^="#bottom"]').on('click',function (e) {
         e.preventDefault();
 
         var target = this.hash,
         $target = $(target);
 
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
+            'scrollBottom': $target.offset().top
         }, 900, 'swing', function () {
             window.location.hash = target;
         });
