3f2f5805ee597b3e0652aa8634c0788a1ef12314
Your Name <you@example.com>
2021-04-13 19:20:00 -0300

    ..

--- src/js/bible.js
+++ src/js/bible.js
@@ -30,7 +30,11 @@ if (params == "") {
         window.location.href = window.location.href+"?book=gênesis&chapter=1"
     }
 }else if(params == 'i=1'){
-    window.location.href = window.location.href+"?book=gênesis&chapter=1"
+    if('params' in localStorage){
+        window.location.href = window.location.href+localStorage.getItem("params")
+    }else{
+        window.location.href = window.location.href+"?book=gênesis&chapter=1"
+    }
 }
 
  // salva ultimo texto lido
